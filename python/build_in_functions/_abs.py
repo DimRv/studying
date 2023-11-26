0fe24fe3d032954f427e9d1638e4a30d27260c74
DimRv <120788544+DimRv@users.noreply.github.com>
2023-11-26 23:33:44 +0300

    abs class

--- python/build_in_functions/_abs.py
+++ python/build_in_functions/_abs.py
@@ -1,3 +1,17 @@
-'''Возвращает абсолютное значение числа'''
+'''Возвращает абсолютное значение объекта'''
 
-print(abs(-4))
+help(abs)
+print(abs(-4), abs(5))
+
+
+class Abs:
+    def __init__(self, val):
+        self.num = val
+
+    def __abs__(self):
+        return abs(self.num)
+
+
+i_abs = Abs(-3)
+
+print(abs(i_abs))
