f23135386fd2624f93fdc85101721b3d793eae63
DimRv <120788544+DimRv@users.noreply.github.com>
2023-12-03 19:53:34 +0300

    03.12.2023

---
+++ python/build_in_functions/_print.py
@@ -0,0 +1,7 @@
+"""
+print(*args, sep=' ', end='\n', file=None, flush=False)
+
+Выводит значения переданных аргуметов функции на стандартный поток вывода sys.stdout"""
+
+help(print)
+print()
---
+++ python/build_in_functions/_type.py
@@ -0,0 +1,38 @@
+"""
+type(object) - возвращает type объекта, переданного в параметре
+type(name, bases, dict, **kwds) - создает новый объект, аргументы становятся значениями __name__, __bases__, __dict__
+
+type один из методов интроспекции (способность исследования объектов внутри самого кода)
+"""
+
+help(type)
+print(type(None))
+print(type(True))
+print(type(2))
+print(type(""))
+print(type(type(None)))
+print('-'*30)
+
+
+d = {
+    'test': 'test',
+    'test2': 'test2',
+}
+TestType = type('TestType', (object,), d)
+
+a = TestType()
+print(a.test, a.test2)
+
+
+class TestType2:
+    test = 'test'
+    test2 = 'test2'
+
+
+a2 = TestType2()
+print(a2.test, a2.test2)
+
+new_var = type(a2)
+a3 = new_var()
+print(a3.test, a3.test2)
+
