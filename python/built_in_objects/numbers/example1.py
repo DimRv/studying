fee19bbe68913b2459a34e2ffb5c330f22c1fbca
DimRv <120788544+DimRv@users.noreply.github.com>
2023-12-09 19:42:44 +0300

    09.12.2023

--- python/built_in_objects/numbers/example1.py
+++ python/built_in_objects/numbers/example1.py
@@ -13,15 +13,15 @@ hour_salary_in30 = round(day_salary_in30/24, 2)
 hour_salary_in31 = round(day_salary_in31/24, 2)
 year_salary = round(real_salary * 12)
 year_tax = round(tax * 12)
-month_to_millioner = 1000000 / real_salary
-print(f'Каждый месяц вы будете отдавать в качестве налога: {tax} рублей')
-print(f'Ваш реальный заработок составит: {real_salary} рублей')
+month_to_millioner = round((1000000 / real_salary, 2))
+print(f'Каждый месяц вы будете отдавать в качестве налога: {tax} рублей.')
+print(f'Ваш реальный заработок составит: {real_salary} рублей.')
 print(f'При 30-дневном месяце средний доход составит: {day_salary_in30} рублей в сутки '
       f'или {hour_salary_in30} в час.')
 print(f'При 31-дневном месяце средний доход составит: {day_salary_in31} рублей в сутки '
       f'или {hour_salary_in31} в час.')
 print(f'За год Вы заработаете {year_salary} рублей, а отдадите налогом {year_tax} рублей.')
-print(f'Если Вы не будете тратить зарплату, миллионером станете через {month_to_millioner} месяцев')
+print(f'Если Вы не будете тратить зарплату, миллионером станете через {month_to_millioner} месяцев.')
 
 
 
