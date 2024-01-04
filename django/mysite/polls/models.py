74a936794627bcea4cfb6dae683bb5df66bde75e
DimRv <120788544+DimRv@users.noreply.github.com>
2024-01-04 17:49:33 +0300

    04.01.2024

--- django/mysite/polls/models.py
+++ django/mysite/polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -16,6 +17,11 @@ else:
         def __str__(self):
             return self.question_text
 
+        @admin.display(
+            boolean=True,
+            ordering="pub_date",
+            description="Published recently?",
+        )
         def was_published_recently(self):
             now = timezone.now()
             return now - datetime.timedelta(days=1) <= self.pub_date <= now
