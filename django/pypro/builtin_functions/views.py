fb22ace91a4f6429cb5da4d387c5764d5d52552b
DimRv <120788544+DimRv@users.noreply.github.com>
2024-01-06 22:54:01 +0300

    06.01.2024

--- django/pypro/builtin_functions/views.py
+++ django/pypro/builtin_functions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from io import StringIO
 import sys
+import importlib
 # Create your views here.
 
 
@@ -35,18 +36,40 @@ def index(request):
 
 
 def detail(request, func_name):
+    sys.path.append('C:\\studying\\python\\built_in_functions')
     func_list = [i for i in __builtins__ if i[0] in [chr(j) for j in range(97, 123)]]
     if func_name not in func_list:
         raise Http404
     with OutputInterceptor() as output:
         help(func_name)
+        i = importlib.import_module(f'_{func_name}')
+    documentation = i.__doc__.strip().split('.')
+    with open(f"C:/studying/python/built_in_functions/_{func_name}.py", encoding='utf-8') as file:
+        comment = False
+        example = []
+        print(file.readlines())
+        for line in file.readlines():
+            if line.find('"""') >= 0:
+                print("!")
+            if line.find('"""') < 0 and not comment:
+                example.append(line)
+            else:
+                if comment:
+                    comment = False
+                else:
+                    comment = True
+        print(example)
+
     description = []
     for i in output[3:]:
         if len(i) > 0:
             description.append(i)
+    print(output)
     context = {'func_name': func_name,
                'syntax': output[2],
-               'description': description,
+               'description': documentation,
                'func_list': func_list,
+               'title': f"Встроенная функция {func_name}",
+               'example': example,
                }
     return render(request, 'builtin_functions/detail.html', context)
