b941e2a949cb998aa5ce1c17a0c622985f7e8486
DimRV <dmitriy.rvantsov@yandex.ru>
2024-02-16 12:49:43 +0300

    16.02.2024

--- python/built_in_functions/_property.py
+++ python/built_in_functions/_property.py
@@ -1,28 +1,41 @@
 """
 property(fget=None, fset=None, fdel=None, doc=None)
 
+Свойства - механизм, который позволяет автоматически запускать методы при обращениях к атрибутам экземпляра.
+В какой то степени это альтернатива __getattr__ и __setattr__/
+По умолчанию к атрибутам класса есть полный доступ на чтение, запись и удаление.
+Свойства могут использоваться для ограничения доступа, при отсутствии сеттера или делитера
+Возвращает новый объект property.
+Обычно геттер у property вычисляет какое-то значение
+
 """
 
 help(property)
 
+print('Пример1'.center(60, "-"))
+
 
 class Test:
+    """Обычный доступ к атрибутам """
+    test = 'test'
+
     def __init__(self):
         self.x = 'start'
 
-    def get_x(self):
-        return self.x
-
 
 t = Test()
-print(t.x)
+print(t.test, t.x)
+t.test = 'new'
 t.x = 'second'
+t.new_attr = 'new_attr'
 print(t.x)
-del t.x
+del t.x, t.test
+
+print('Пример2'.center(60, "-"))
 
 
 class Test2:
-    """Свойство защищено от перезаписи и удаления"""
+    """Свойство защищено от перезаписи и удаления при отсутствии сеттера и делитера"""
     @property
     def x(self):
         return 'test'
@@ -31,26 +44,31 @@ t2 = Test2()
 print(t2.x)
 print(Test2.x, type(Test2.x))
 print(dir(Test2.x))
-#t2.x = '1'
-#del t2.x
+#t2.x = '1' - ошибка
+#del t2.x - ошибка
+
+print('Пример3'.center(60, "-"))
 
 
 class Test3:
-    """Свойство защищено от удаления"""
-    __y = 'test'
+    """Свойство защищено только от удаления"""
 
     def __init__(self):
         self.n = 'start'
 
     @property
     def x(self):
-        return self.x
+        return self.n + ' get'
 
     @x.setter
     def x(self, x):
-        self.x = x
+        self.n = x + ' set'
 
 
 t3 = Test3()
-print(t3.__y)
+print(t3.x)
+t3.x = 'end'
+print(t3.x)
+t3.n = 'some'
+print(t3.x)
 
--- python/built_in_types/_object.py
+++ python/built_in_types/_object.py
@@ -1,10 +1,14 @@
-"""object - основной объект от которого наследуются все другие"""
+"""
+object - основной type от которого наследуются все другие
+
+От него наследуется 89 встроенных подклассов
+"""
 
 help(object)
 a = object()
 b = object()
 print(a, type(a), dir(a))
 #a.attr = 'val' - error
-print(a==b)
+print(a == b)
 
 
