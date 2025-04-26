6d3e6ef0ccd10ad3d90a6d082e121f38c0a41251
DimRv <120788544+DimRv@users.noreply.github.com>
2025-04-26 14:21:45 +0300

    26.04.2025

--- python/built_in_functions/_dict.py
+++ python/built_in_functions/_dict.py
@@ -1,7 +1,5 @@
 """
-dict()
-Встроенная функция для создания словаря
-
+Встроенная функция для создания словаря или преобразования итерируемых объектов к словарю
 """
 
 help(dict)
@@ -9,7 +7,7 @@ help(dict)
 
 d1 = dict()
 d2 = dict([('test', 'hi'), ('test2', 'hello')])
-d3 = dict(test='hi')
+d3 = dict(test='hi', age=9, is_baby=True)
 
 print(d1, type(d1))
 print(d2, type(d2))
